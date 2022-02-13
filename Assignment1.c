e300966175aa9cd6b6ac49c072d0fc977b470472
rmadhabachary <99424197+rmadhabachary@users.noreply.github.com>
2022-02-13 18:37:09 +0530

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+int main() {
+
+    char name[50];
+    printf("Enter your name:\n ");
+    scanf("%s",name);
+
+    char branch[50];
+    printf("Enter your branch name:\n ");
+    scanf("%s",branch);
+
+    int regno[50];
+    printf("Enter your reg. no.\n ");
+    scanf("%s",regno);
+
+    char hobby[100];
+    printf("Write your hobbies here:\n ");
+    scanf("%s",hobby);
+    
+    return 0;
+}
